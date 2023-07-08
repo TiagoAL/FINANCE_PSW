f4a9bda688743a7f00c02973085b412496c5b1cc
Tiago Alves de Lima <tiagoalveslima6@gmail.com>
2023-07-07 22:19:40 -0300

    terceira aula da pystack week 7.0 - FINAN.CE

---
+++ contas/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import ContaPaga, ContaPagar
+
+admin.register(ContaPaga)
+admin.register(ContaPagar)
--- perfil/views.py
+++ perfil/views.py
@@ -1,14 +1,35 @@
+from datetime import datetime
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Conta, Categoria
 from django.contrib import messages
 from django.contrib.messages import constants
-from .utils import calcula_total
+from .utils import calcula_total, calcula_equilibrio_financeiro
+from extrato.models import Valores
+from django.db.models import Sum
 
 def home(request):
+    valores = Valores.objects.filter(data__month=datetime.now().month)
+    entradas = valores.filter(tipo='E')
+    saidas = valores.filter(tipo='S')
+    
+    total_entradas = calcula_total(entradas, 'valor')
+    total_saidas = calcula_total(saidas, 'valor')
+    
     contas = Conta.objects.all()
     saldo_total = calcula_total(contas, 'valor')
-    return render(request, 'home.html', {'contas': contas, 'saldo_total': saldo_total})
+    
+    percentual_gastos_essenciais, percentual_gastos_nao_essenciais = calcula_equilibrio_financeiro()
+    return render(request, 'home.html', 
+        {
+            'contas': contas, 
+            'saldo_total': saldo_total, 
+            'total_entradas': total_entradas, 
+            'total_saidas': total_saidas,
+            'percentual_gastos_essenciais': int(percentual_gastos_essenciais),
+            'percentual_gastos_nao_essenciais': int(percentual_gastos_nao_essenciais)
+        }
+    )
 
 def gerenciar(request):
     contas = Conta.objects.all()
@@ -69,3 +90,12 @@ def update_categoria(request, id):
     categoria.save()
     
     return redirect('/perfil/gerenciar/')
+
+def dashboard(request):
+    dados = {}
+    categorias = Categoria.objects.all()
+    
+    for categoria in categorias:
+        dados[categoria.categoria] = Valores.objects.filter(categoria=categoria).aggregate(Sum('valor'))['valor__sum']
+    
+    return render(request, 'dashboard.html', {'labels': list(dados.keys()), 'values': list(dados.values())})
